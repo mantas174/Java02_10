999be1eb4ff8f1c7a79712a300f06ffa584481c4
Mantas <mantas.muzzz@gmail.com>
2018-01-30 16:06:35 +0200

    Padariau

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,6 +7,13 @@ public class Main {
 	        Sukurti skaičiuotuvo klasę, kurioje realizuoti statinius metodus sumai, skirtumui,
             sandaugai. Per parametrus bus naudojami sveiko tipo skaičiai.
 	     */
+        int a, b;
+        a = 5;
+        b = 6;
+        Skaiciuotuvas objektas = new Skaiciuotuvas();
 
+        System.out.println("Suma: " + objektas.suma(a, b));
+        System.out.println("Skirtumas: " + objektas.skirtumas(a, b));
+        System.out.println("Sandauga: " + objektas.sandauga(a, b));
     }
 }
---
+++ src/com/company/Skaiciuotuvas.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Skaiciuotuvas {
+    public static int suma(int a, int b){
+        return a + b;
+    }
+    public static int skirtumas(int a, int b){
+        return a - b;
+    }
+    public static int sandauga(int a, int b){
+        return a * b;
+    }
+}
