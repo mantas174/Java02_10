a204b51331d816072d819dab1c476de8b8ff4172
Mantas <mantas.muzzz@gmail.com>
2018-01-30 16:02:57 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+	        Sukurti skaičiuotuvo klasę, kurioje realizuoti statinius metodus sumai, skirtumui,
+            sandaugai. Per parametrus bus naudojami sveiko tipo skaičiai.
+	     */
+
+    }
+}
